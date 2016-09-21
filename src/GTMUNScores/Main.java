b4836f59e66f4f5dba429ce1b381c1f276eb79e6
Tiffany R Zhou <tiffanyrzhou@gmail.com>
2016-09-21 10:39:32 -0400

    File Upload Feature

--- src/GTMUNScores/Main.java
+++ src/GTMUNScores/Main.java
@@ -5,14 +5,49 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextAreaBuilder;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class Main extends Application {
+    File scoresFile;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("GTMUNScores.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Label status = new Label("Welcome to GTMUN 2016 Scoring Application");
+        status.setFont(new Font(20));
+        Button buttonLoad = new Button("Upload File");
+        buttonLoad.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                FileChooser fileChooser = new FileChooser();
+                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+                fileChooser.getExtensionFilters().add(extFilter);
+                scoresFile = fileChooser.showOpenDialog(primaryStage);
+            }
+        });
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(status, buttonLoad);
+        root.getChildren().add(vBox);
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
     }
 
