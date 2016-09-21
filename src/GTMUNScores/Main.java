1217f16fb5acfb2996ad8be3a939c83a26e1137f
Tiffany R Zhou <tiffanyrzhou@gmail.com>
2016-09-21 10:46:52 -0400

    Session Object Created

--- src/GTMUNScores/Main.java
+++ src/GTMUNScores/Main.java
@@ -5,11 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
@@ -17,12 +15,10 @@ import javafx.scene.control.TextAreaBuilder;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.VBoxBuilder;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
---
+++ src/GTMUNScores/Session.java
@@ -0,0 +1,7 @@
+package GTMUNScores;
+
+/**
+ * Created by Tiffany on 9/21/16.
+ */
+public class Session {
+}
